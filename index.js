2050e164a43e7f13e96e8b8139a585ed071bdafe
Joao Marcelo <joao.mm.santos@outlook.com>
2025-02-18 17:09:15 -0300

    swagger doc

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import connectDatabase from './src/database/db.js';
 import userRoute from './src/routes/user.route.js';
 import authRoute from './src/routes/auth.route.js';
 import newsRoute from './src/routes/news.route.js';
+import swaggerRoute from './src/routes/swagger.route.js';
 
 
 const port = process.env.PORT || 3000;
@@ -15,5 +16,6 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
+app.use("/doc", swaggerRoute);
 
 app.listen(port, () => console.log(`Server is running in localhost:${port}`));
